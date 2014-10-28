b052f5686c357702791087b1376ea34ffadc13ce
Helbert <helbert@debian.home>
2014-10-28 21:05:01 -0200

    Alocação de Memória

--- oc8top.c
+++ oc8top.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include "mongoose.h"
 int main(int argc, char **argv) {
-   fprintf(stdout, "EC8TOP 23-10-14\n");
-   return EXIT_SUCCESS;
+   	fprintf(stdout, "EC8TOP 23-10-14\n");
+	char buffer[100];
+	return EXIT_SUCCESS;
 }
 
 
