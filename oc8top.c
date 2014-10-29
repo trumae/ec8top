4c0622ce1ff0e3f3efb894b4784e7c451e244b9e
Helbert <helbert@debian.home>
2014-10-29 19:52:18 -0200

    Criação da Função memoria

--- oc8top.c
+++ oc8top.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mongoose.h"
+
+int memoria(char *b, size_t n){
+	system("df");
+}
+
 int main(int argc, char **argv) {
    	fprintf(stdout, "EC8TOP 23-10-14\n");
 	char buffer[100];
+	memoria(buffer,100);
 	return EXIT_SUCCESS;
 }
 
