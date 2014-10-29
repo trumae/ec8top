267fbbbe9b00c78bb6af4974b29985fe3fcdbf21
Leandro <leandro@debianLMS>
2014-10-29 17:38:23 -0200

    primeira funcao de teste

--- oc8top.c
+++ oc8top.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mongoose.h"
+
+int processos(char *b, size_t n){
+	system("top");
+
+}
 int main(int argc, char **argv) {
-   fprintf(stdout, "EC8TOP 23-10-14\n");
+	fprintf(stdout, "EC8TOP 23-10-14\n");
+	char buffer[100];
+	processos(buffer,100);
+
    return EXIT_SUCCESS;
 }
 
