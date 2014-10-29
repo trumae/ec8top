d23ed7a4c6317d115e06d7fdea6f654e8a3f709f
Trunmae <trumae@gmail.com>
2014-10-29 12:45:54 -0500

    oc8top changed

--- oc8top.c
+++ oc8top.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include "mongoose.h"
-int main(int argc, char **argv) {
-   fprintf(stdout, "EC8TOP 23-10-14\n");
-   return EXIT_SUCCESS;
+
+static int ev_handler(struct mg_connection *conn, enum mg_event ev) {
+  switch (ev) {
+    case MG_AUTH: return MG_TRUE;
+    case MG_REQUEST:
+      mg_send_header(conn, "Content-Type", "text/html");
+      mg_printf_data(conn, "<doctype html>"
+	"<html>"
+        "<header><title>EC8</title></header>"
+        "<body>"
+        "<center><h1>Galo Doido</h1></center>"
+        "</body>"
+        "</html>");
+      return MG_TRUE;
+    default: return MG_FALSE;
+  }
 }
 
+int main(void) {
+  struct mg_server *server;
 
+  // Create and configure the server
+  server = mg_create_server(NULL, ev_handler);
+  mg_set_option(server, "listening_port", "8080");
+
+  // Serve request. Hit Ctrl-C to terminate the program
+  printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
+  for (;;) {
+    mg_poll_server(server, 1000);
+  }
+
+  // Cleanup, and free server instance
+  mg_destroy_server(&server);
+
+  return 0;
+}
