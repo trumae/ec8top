567a8f4bf4e608bb9a21fe9944b7bffc31cac8ac
Helbert <helbert@debian.home>
2014-10-29 20:19:33 -0200

    Criação da Função memoria

--- oc8top.c
+++ oc8top.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>//biblioteca para poder usar system
 #include "mongoose.h"
 
 int memoria(char *b, size_t n){
-	system("df");
+	system("df");//comando df para mostrar memória livre
 }
 
 int main(int argc, char **argv) {
    	fprintf(stdout, "EC8TOP 23-10-14\n");
-	char buffer[100];
+	char buffer[100];//criado para a alocação de memória
 	memoria(buffer,100);
 	return EXIT_SUCCESS;
 }
