1248fd6abb5736f0c2f2ab8f634b381d0ffdceeb
Leandro Matos <lms_91@hotmail.com>
2014-10-30 20:39:38 -0200

    correcao da acentuacao html

--- versaoso.c
+++ versaoso.c
@@ -11,9 +11,9 @@ int versaoso(char *b, size_t s) {//Função mostra na tela os dados da versão d
   }else{   
     char buffer[500];
     buffer[0] = '\x0';
-    snprintf(buffer,500, "S.O.: %s<br>Nome personalizado: %s<br>Release: %s<br>Versão: %s<br>Arquitetura: %s<br>",
+    snprintf(buffer,500, "S.O.: %s<br>Nome personalizado: %s<br>Release: %s<br>Vers&atildeo: %s<br>Arquitetura: %s<br>",
        my_uname.sysname, my_uname.nodename, my_uname.release,my_uname.version,my_uname.machine);
-    strncat(b, "<h4>Versão do Sistema Operacional</h4>", s);
+    strncat(b, "<h4>Vers&atildeo do Sistema Operacional</h4>", s);
     strncat(b, buffer, s);
   }
 	return 1;
