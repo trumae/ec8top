547e6c05e4a6e07108ce63af25eca657b5985ea0
Trunmae <trumae@gmail.com>
2014-11-21 11:08:18 -0600

    versao inicial para android

---
+++ android/src/org/ec8/ec8top/MainActivity.java
@@ -0,0 +1,22 @@
+package org.ec8.ec8top;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        startHTTP();
+    }
+
+    private native String startHTTP();
+
+    static {
+        System.loadLibrary("ec8top");
+    }
+}
