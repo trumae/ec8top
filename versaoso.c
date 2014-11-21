c4b392fd787365696f170d044cb48398ee86e7a8
Nilson <nilson.pessoal@gmail.com>
2014-11-20 22:39:03 -0200

    alterado layout na funcao versaoso

--- versaoso.c
+++ versaoso.c
@@ -11,9 +11,9 @@ int versaoso(char *b, size_t s) {//Função mostra na tela os dados da versão d
   }else{   
     char buffer[500];
     buffer[0] = '\x0';
-    snprintf(buffer,500, "S.O.: %s<br>Nome personalizado: %s<br>Release: %s<br>Vers&atildeo: %s<br>Arquitetura: %s<br>",
+    snprintf(buffer,500, "<p>S.O.: %s<br>Nome personalizado: %s<br>Release: %s<br>Vers&atildeo: %s<br>Arquitetura: %s</p>",
        my_uname.sysname, my_uname.nodename, my_uname.release,my_uname.version,my_uname.machine);
-    strncat(b, "<h4>Vers&atildeo do Sistema Operacional</h4>", s);
+    strncat(b, "<h2>Vers&atildeo do Sistema Operacional</h2>", s);
     strncat(b, buffer, s);
   }
 	return 1;
