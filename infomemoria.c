c8c9d190aa659074799654f0f65bb44b518bfea4
Nilson <nilson.pessoal@gmail.com>
2014-11-20 22:28:03 -0200

    alterado layout da funcao infmemoria

--- infomemoria.c
+++ infomemoria.c
@@ -16,9 +16,9 @@ int infomemoria(char *b, size_t s) {//Função que que mostra status da memoria 
 	buffer[0] = '\x0';
 	
 	/*impressao das informacoes*/
-	snprintf(buffer,500, "Total de Mem&oacuteria RAM: %5.1f MB<br>Total de Mem&oacuteria RAM livre: %5.1f MB<br>Total de Mem&oacuteria 			RAM em uso: %5.1f MB<br>",
+	snprintf(buffer,500, "<p>Total de Mem&oacuteria RAM: %5.1f MB<br>Total de Mem&oacuteria RAM livre: %5.1f MB<br>Total de Mem&oacuteria 			RAM em uso: %5.1f MB</p>",
        		si.totalram / megabyte,si.freeram / megabyte , (si.totalram - si.freeram) / megabyte);
-	strncat(b, "<h4>Informa&ccedil&otildees sobre a Mem&oacuteria RAM: </h4>", s);
+	strncat(b, "<h2>Informa&ccedil&otildees sobre a Mem&oacuteria RAM: </h2>", s);
 	strncat(b, buffer, s);
 
   
