99282781eaf1cfe5727ef5de59fd357289132c08
Helbert Gomes <htg.cam@hotmail.com>
2014-12-01 23:06:59 -0200

    Alterado desing para imprimir o resultado em html

--- numprocessos.c
+++ numprocessos.c
@@ -18,7 +18,7 @@ int numprocessos(char *b, size_t s) {//Função que que mostra quantos processos
 	buffer[0] = '\x0';
 	
 	/*impressao das informacoes*/
-	snprintf(buffer,500, "<b>UpTime:</b> <br>%ld days, %ld:%02ld:%02ld hours<br> <b>Number of Processes:</b> <br>%d Processes",
+	snprintf(buffer,500, "<b>UpTime:</b> %ld days, %ld:%02ld:%02ld hours<br> <b>Number of Processes:</b> <br>%d Processes",
        		si.uptime / dia , (si.uptime % dia) / hora,(si.uptime % hora) / minuto , si.uptime % minuto , si.procs);
 	
 	//coloca as informacoes no buffer do projeto
