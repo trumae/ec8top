24aba50855fef111a99dcfa7f1c84de573ff4a98
Helbert Gomes <htg.cam@hotmail.com>
2014-12-03 22:03:30 -0200

    corrigido erros ortográficos

--- load_avarege.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LOAD_AVAREGE
-
-#define LINUX_SYSINFO_LOADS_SCALE 65536 //necessário para definir a escala devido ao 2¹⁶
-
-int load_avarege(char *b, size_t s); //assinatura da função
-
-#endif
